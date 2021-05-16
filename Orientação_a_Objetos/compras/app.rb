b749739168076a5617a9e2d009ad623e3b975149
Caio André <caio.andre01@gmail.com>
2021-05-16 11:53:13 -0300

    Added one new product

--- Orientação_a_Objetos/compras/app.rb
+++ Orientação_a_Objetos/compras/app.rb
@@ -4,5 +4,8 @@ require_relative 'produto'
 
 
 macarrao = Product.new("Macarrão",50.0)
+arroz = Product.new("Arroz",20.0)
 mrkt_1 = Market.new(macarrao)
+markt_2 = Market.new(arroz)
 puts mrkt_1.buy
+puts markt_2.buy
