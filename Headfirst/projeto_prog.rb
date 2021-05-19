c1404f42e2303f69b4d89d1d9a7d2f030856a33e
Henrique Silva <josehs555@gmail.com>
2021-05-18 22:31:45 -0300

    Algumas alterações

--- Headfirst/projeto_prog.rb
+++ Headfirst/projeto_prog.rb
@@ -1,16 +1,47 @@
-class Blender 
-    def close_lid
-    puts "Sealed tight!"
-    end 
+class Livro
 
-    def blend (speed)
-        puts "Spinnig on #{speed} setting."
-    end 
-
-end 
-a = getshe
-puts "hello"
-blender = Blender.new
-blender.close_lid
-blender.blend("high")
-
+    attr_accessor :titulo, :autor, :categoria, :paginas, :preco
+  
+    def initialize(genero, titulo, autor, paginas, preco)
+      @genero, @titulo, @autor, @paginas, @preco = genero, titulo, autor, paginas, preco
+    end
+  
+    def ==(other)
+      self.titulo == other.titulo && self.autor == other.autor && self.genero == other.genero && self.paginas == other.paginas && self.preco == other.preco
+    end
+  
+  
+    def to_s
+      "Gênero: #{@genero}; Titulo: #{@titulo}; Autor: #{@autor}; Paginas: #{@paginas}; Preco: #{@preco}"
+    end
+  
+  end
+  
+  class Estante
+    def initialize
+        @livros = []
+    end
+  
+    def filtrar 
+      
+    end
+  
+    def mostrar_livros_guardados
+        puts "\n"
+        @livros.each_key {|categoria|
+          for livro in @livros[categoria]
+            puts livro
+          end
+        }
+    end
+  
+    def mostrar_livros_da_categoria categoria
+      if @livros.has_key?categoria
+        for livro in @livros[categoria]
+          puts livro
+        end
+      else
+        puts "Esta categoria não possui livros nesta biblioteca"
+      end
+    end
+  end
