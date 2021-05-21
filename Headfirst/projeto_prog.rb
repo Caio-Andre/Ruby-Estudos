6ab72928ea917a39e2a4831bd0906ae925b87a98
Henrique Silva <josehs555@gmail.com>
2021-05-21 20:10:27 -0300

    bora ver

--- Headfirst/projeto_prog.rb
+++ Headfirst/projeto_prog.rb
@@ -29,14 +29,38 @@ class Estante
   end
 end
 
-def adicionar_livros_banco_de_dados ()
-  genero = gets.chomp
-  File.open("banco_de_dados.txt", "a") do |file|
-    file.puts("#{genero}|#{livro_1.titulo}|#{livro_1.autor}|#{livro_1.paginas}|#{livro_1.preco}")
-  end 
+# ADICIONA NOVOS LIVROS NO BANCO
+def adicionar_livros_banco_de_dados
+  while true
+    print "Por favor, informe o gênero do livro:"
+    id = gets.chomp.to_i
+    print "Por favor, informe o gênero do livro:"
+    genero = gets.chomp
+    print "Por favor, informe o título do livro:"
+    titulo = gets.chomp
+    print "Por favor, informe o autor do livro:"
+    autor = gets.chomp
+    print "Por favor, informe o número de páginas do livro:"
+    paginas = gets.chomp.to_i
+    print "Por favor, informe o preço do livro:"
+    preco = gets.chomp.to_f
+
+    if genero.class == String && titulo.class == String
+
+    File.open("banco_de_dados.txt", "a") do |arquivo|
+      arquivo.puts("#{genero}|#{titulo}|#{autor}|#{paginas}|#{preco}")
+    end
+    
+    puts "Livro adicionado com sucesso"
+    puts "Você deseja adicionar mais livros?"
+    puts "Se você não deseja adicionar mais livros, digite 'N'."
+    if gets.chomp.upcase == 'N'
+      break
+    end
+  end
 end 
 
-# lER ESSE CARALHO
+# LER OS LIVROS DA ESTANTE
 
 def carregar_livros
   estante = []
