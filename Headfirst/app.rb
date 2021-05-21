7d8fdd4d5187cfc8d530df53f67405109d92691b
Henrique Silva <josehs555@gmail.com>
2021-05-21 20:20:03 -0300

    paciência

--- Headfirst/app.rb
+++ Headfirst/app.rb
@@ -1,13 +1,37 @@
 require_relative 'class_livro'
 require_relative 'class_estante'
 
+# ADICIONA NOVOS LIVROS NO BANCO
+def adicionar_livros_banco_de_dados
+  while true
+    print "Por favor, informe o gênero do livro:"
+    id = gets.chomp.to_i
+    print "Por favor, informe o gênero do livro:"
+    genero = gets.chomp
+    print "Por favor, informe o título do livro:"
+    titulo = gets.chomp
+    print "Por favor, informe o autor do livro:"
+    autor = gets.chomp
+    print "Por favor, informe o número de páginas do livro:"
+    paginas = gets.chomp.to_i
+    print "Por favor, informe o preço do livro:"
+    preco = gets.chomp.to_f
 
-def adicionar_livros_banco_de_dados ()
-  genero = gets.chomp
-  File.open("banco_de_dados.txt", "a") do |file|
-    file.puts("#{genero}|#{livro_1.titulo}|#{livro_1.autor}|#{livro_1.paginas}|#{livro_1.preco}")
-  end 
+    if genero.class == String && titulo.class == String
+
+    File.open("banco_de_dados.txt", "a") do |arquivo|
+      arquivo.puts("#{genero}|#{titulo}|#{autor}|#{paginas}|#{preco}")
+    end
+    
+    puts "Livro adicionado com sucesso"
+    puts "Você deseja adicionar mais livros?"
+    puts "Se você não deseja adicionar mais livros, digite 'N'."
+    if gets.chomp.upcase == 'N'
+      break
+    end
+  end
 end 
+
 
 # Ler os livros do banco de dados e cria objetos da class Livros. 
 
