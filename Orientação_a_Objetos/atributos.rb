0c791858cae9e42bf035f51aafd675fad801ad4f
cCaio-byte <62912809+cCaio-byte@users.noreply.github.com>
2021-06-19 10:59:19 -0300

    changed files

--- Orientação_a_Objetos/atributos.rb
+++
@@ -1,9 +0,0 @@
-class Dog
-    attr_accessor :name, :age
-end 
-
-dog = Dog.new
-dog.name = "Marlon"
-dog.age = 10
-puts dog.name
-p dog.age
--- Orientação_a_Objetos/variaveis/local.rb
+++
@@ -1,9 +0,0 @@
-def foo
-    local = 'local é acessada apenas dentro deste método'
-    p local
-end 
-
-foo
-
-local = "\nMesmo nome de variável e n ocorre erro"
-puts local
